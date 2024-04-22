bd3cd5bee6074b13e34e28d9dc5905fc79759681
Akhildathu <44724040+Akhildathu@users.noreply.github.com>
2024-04-22 11:55:12 -0500

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-1.cluster-c5a606sc0cen.us-east-1.rds.amazonaws.com',
+    DB_USER : 'akhil',
+    DB_PWD : 'akhil123',
+    DB_DATABASE : 'webappdb'
 });
