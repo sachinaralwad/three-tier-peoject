e8df5ca323f65f0088bc2b49a08d01496e3127b8
shaikashir <131173777+shiakashir@users.noreply.github.com>
2024-08-22 14:57:41 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-1.cluster-cp0ioq0esij9.ap-southeast-2.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : '12345Rockey',
+    DB_DATABASE : 'database-1'
 });
